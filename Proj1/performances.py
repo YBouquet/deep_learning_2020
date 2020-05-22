8e5f7bde16226ceac8f7167cc296b619f81d5169
YBouquet <y.bouquet29@gmail.com>
2020-05-22 21:15:53 +0200

    uncomment performance scenarios

--- Proj1/performances.py
+++ Proj1/performances.py
@@ -9,17 +9,17 @@ This code generate the script that run all configurations used to develop the re
 
 #this dictionary defines all the configuration that we run to obtain the results given in the main development of the report
 GETTERS_DICT =  {
-                    #'net':                                  {'model' : 'net', 'criterion' : 'mse','optimizer' : 'sgd', 'rate' : '2e-3'}, #model given in the practicals
-                    #'net2':                                 {'model' : 'net2','criterion' : 'mse','optimizer' : 'sgd', 'rate' : '5e-3'}, #model given in the practicals
-                    #'lenet5':                               {'model' : 'lenet5','criterion' : 'mse','optimizer' : 'sgd', 'rate' : '5e-3'}, #LeCun's model with correction due to different input format
-                    #'2nets_sgd':                            {'model' : '2nets','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
-                    #'2nets_ws_sgd':                         {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
-                    #'2nets_ws_aux_sgd':                     {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '1.', 'weight_decay' : '0.'},
-                    #'2nets_ws_aux_adam':                    {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'adam', 'rate' : '7e-5', 'beta': "0.5", 'weight_auxiliary_loss': '1.', 'weight_decay' : '0.'},
-                    #'2nets_ws_aux_do_adam':                 {'model' : '2nets_ws_do','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.6", 'weight_auxiliary_loss': '1.','weight_decay' : '0.'},
-                    #'2nets_ws_aux_do_bn_adam':              {'model' : '2nets_ws_bn','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.6", 'weight_auxiliary_loss': '1.','weight_decay' : '0.'},
-                    #'2netws_ws_aux_do_wd_adam':             {'model' : '2nets_ws_do','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.9", 'weight_auxiliary_loss': '1.','weight_decay' : '1e-5'},
-                    #'required':                             {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'adam', 'rate' : '4e-5', 'beta': "0.6", 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
+                    'net':                                  {'model' : 'net', 'criterion' : 'mse','optimizer' : 'sgd', 'rate' : '2e-3'}, #model given in the practicals
+                    'net2':                                 {'model' : 'net2','criterion' : 'mse','optimizer' : 'sgd', 'rate' : '5e-3'}, #model given in the practicals
+                    'lenet5':                               {'model' : 'lenet5','criterion' : 'mse','optimizer' : 'sgd', 'rate' : '5e-3'}, #LeCun's model with correction due to different input format
+                    '2nets_sgd':                            {'model' : '2nets','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
+                    '2nets_ws_sgd':                         {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
+                    '2nets_ws_aux_sgd':                     {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'sgd', 'rate' : '5e-3', 'weight_auxiliary_loss': '1.', 'weight_decay' : '0.'},
+                    '2nets_ws_aux_adam':                    {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'adam', 'rate' : '7e-5', 'beta': "0.5", 'weight_auxiliary_loss': '1.', 'weight_decay' : '0.'},
+                    '2nets_ws_aux_do_adam':                 {'model' : '2nets_ws_do','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.6", 'weight_auxiliary_loss': '1.','weight_decay' : '0.'},
+                    '2nets_ws_aux_do_bn_adam':              {'model' : '2nets_ws_bn','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.6", 'weight_auxiliary_loss': '1.','weight_decay' : '0.'},
+                    '2netws_ws_aux_do_wd_adam':             {'model' : '2nets_ws_do','criterion' : 'ce','optimizer' : 'adam', 'rate' : '6e-4', 'beta': "0.9", 'weight_auxiliary_loss': '1.','weight_decay' : '1e-5'},
+                    'required':                             {'model' : '2nets_ws','criterion' : 'ce','optimizer' : 'adam', 'rate' : '4e-5', 'beta': "0.6", 'weight_auxiliary_loss': '0.', 'weight_decay' : '0.'},
                     '2nets_ws_aux_do_wd_adam_hyperparameters' :{'model' : '2nets_ws_do','criterion' : 'ce','optimizer' : 'adam', 'rate' : '1e-4', 'beta': "0.9", 'weight_auxiliary_loss': '1.','weight_decay' : '0.'}
                 }
 
